584d7eab75c1944b1cee46d8d29a25f1363bdc58
Raj Rana <be2020se715@gces.edu.np>
2024-07-04 20:12:17 +0545

    day3?

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,26 +1,61 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
 // require is used to include nodules . fs nodule is used to work on files.
+
+///////////////////////////
+////FILES
+
 //FS stands for File System
 
 //Blocking synchronous way
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("text written");
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("text written");
 
-//Non-blocking asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+// //Non-blocking asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written");
-      });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+
+////////////////////
+////SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is overview");
+  } else if (pathName === "/product") {
+    res.end("This is product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
     });
-  });
+    res.end("<h1>Page not found<h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port 8000");
 });
